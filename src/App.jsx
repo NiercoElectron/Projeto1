b8d7d2897a61c716eed6a867bf3e75b983495c90
Enrico <vincentwelles@protonmail.com>
2026-03-09 11:42:19 -0300

    feat: chamando os componenetes no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
+import Header from "./components/Header"
+import Resultado from "./components/Resultado"
 
 function App() {
 
 
   return (
     <>
-
+      <Header/>
+      <Resultado/>
     </>
   )
 }
